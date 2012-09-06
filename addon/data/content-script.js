07868ebf2b2d2ba7745d176d93da4787aa324e40
Myk Melez <myk@mozilla.org>
2012-09-06 16:59:12 -0700

    explain what the content script does

--- addon/data/content-script.js
+++ addon/data/content-script.js
@@ -1,3 +1,9 @@
+// This content script is intended to be a dump pipe, as the content is trusted,
+// and we'd connect it directly to the addon if we could.  Thus we simply
+// forward all messages we receive in either direction, and we use postMessage()
+// to communicate with the addon, to maintain consistency between
+// the content<->content-script and content-script<->addon transmissions.
+
 document.defaultView.addEventListener("message", function(event) {
   self.postMessage(event.data);
 }, false);
