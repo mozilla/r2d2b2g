40f0c25c6113778865418ea7458128e6bda9a75f
Myk Melez <myk@mozilla.org>
2012-09-06 14:59:19 -0700

    make executable launch work on Mac

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -5,6 +5,7 @@ const { Cc, Ci, Cr, Cu } = require("chrome");
 require("widget");
 require("self");
 require("url");
+require("runtime");
 
 require("widget").Widget({
   id: "r2d2b2g",
@@ -12,8 +13,16 @@ require("widget").Widget({
   content: "r2d2b2g",
   width: 50,
   onClick: function() {
+    let executables = {
+      WINNT: "b2g/b2g.exe",
+      Darwin: "mac64/B2G.app/Contents/MacOS/b2g",
+      Linux: "",
+    };
+    let url = require("self").data.url(executables[require("runtime").OS]);
+    let path = require("url").toFilename(url);
+
     let b2g = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
-    b2g.initWithPath(require("url").toFilename(require("self").data.url("b2g/b2g.exe")));
+    b2g.initWithPath(path);
 
     let profile = require("url").toFilename(require("self").data.url("profile"));
     let args = ["-profile", profile];
