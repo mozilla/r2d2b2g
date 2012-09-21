17f6456a669d00f08e81e790d6b39c99cdd718e5
Myk Melez <myk@mozilla.org>
2012-09-20 17:27:13 -0700

    allow you to specify platform of B2G Desktop build to download

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 .PHONY: help build run package
 
+ifdef PLATFORM
+PLATFORM_ARG = --platform $(PLATFORM)
+endif
+
+
 build:
 	cd gaia && make
 	rm -rf gaia/profile/startupCache
 	rm -rf addon/data/profile && mv gaia/profile addon/data/profile
-	python ./build.py
+	python ./build.py $(PLATFORM_ARG)
 
 run:
 	cd addon-sdk && source bin/activate && cd ../addon && cfx run
--- build.py
+++ build.py
@@ -7,6 +7,7 @@ import os
 import sys
 import tempfile
 import shutil
+from optparse import OptionParser
 
 sys.path.insert(0, os.path.join(os.getcwd(), "mozdownload"))
 sys.path.insert(0, os.path.join(os.getcwd(), "mozbase", "mozinstall"))
@@ -15,6 +16,16 @@ sys.path.insert(0, os.path.join(os.getcwd(), "mozbase", "mozinfo"))
 from mozdownload.scraper import DailyScraper
 import mozinstall
 
+usage = 'usage: %prog [options]'
+parser = OptionParser(usage=usage, description=__doc__)
+parser.add_option('--platform', '-p',
+                  dest='platform',
+                  choices=['win32', 'mac64', 'linux'],
+                  metavar='PLATFORM',
+                  help='platform of the B2G Desktop build to download; '
+                       'default: current platform')
+(options, args) = parser.parse_args()
+
 #tmpdir = tempfile.mkdtemp()
 #print "temp dir: %S", tmpdir
 #downloaddir = tmpdir
@@ -22,20 +33,29 @@ downloaddir = os.getcwd()
 
 datadir = os.path.join(os.getcwd(), "addon", "data")
 
-if sys.platform == 'win32':
-  platform = 'win32'
+if options.platform:
+  platform = options.platform
+else:
+  if sys.platform == 'win32':
+    platform = 'win32'
+  elif sys.platform == 'darwin':
+    platform = 'mac64'
+  elif sys.platform.startswith('linux'):
+    platform = 'linux'
+  else:
+    platform = sys.platform
+
+if   platform == 'win32':
   file_extension = '.zip'
   installdirname = 'b2g'
-elif sys.platform == 'darwin':
-  platform = 'mac64'
+elif platform == 'mac64':
   file_extension = '.dmg'
   installdirname = 'B2G.app'
-elif sys.platform.startswith('linux'):
-  platform = 'linux'
+elif platform == 'linux':
   file_extension = '.tar.bz2'
   installdirname = 'b2g'
 else:
-  raise NotImplementedError('platform %s not supported' % sys.platform)
+  raise NotImplementedError('platform %s not supported' % platform)
 
 # Download latest build of B2G Desktop.
 
