bbb0549f9ac5f33ef9b545ec4062fea94735f695
Harald Kirschner <hkirschner@mozilla.com>
2012-10-22 15:59:29 -0700

    - Revamped sidebar with proper CSS3-driven simulator toggle  - Better section switcher code, allows showing #welcome page in first launch!

--- addon/data/content/js/main.js
+++ addon/data/content/js/main.js
@@ -1,55 +1,78 @@
-$(document).ready(function() {
+var Simulator = {
 
-  var section = $('#index')[0];
-  $(document).delegate('nav a', 'click', function(evt) {
-    var target = $(this).attr('href');
+  init: function() {
+
+    this.toggler = $('#command-toggle')[0];
+    $(this.toggler).on('change', function(evt) {
+      // FIXME: Change to actual checkbox state
+      Simulator.toggle();
+    });
+
+    var current = document.location.hash.substr(1) || 'index';
+    Simulator.show('#' + current);
+
+    $(document).on('click', 'a[href^="#"]', function(evt) {
+      var target = $(this).attr('href');
+      if ($(target)[0].tagName.toLowerCase() == 'section') {
+        Simulator.show(target);
+      }
+    });
+
+    document.documentElement.addEventListener(
+      "addon-message",
+      function isRunningListener(event) {
+        var message = event.detail;
+        if (!("name" in message)) {
+          return;
+        }
+        switch (message.name) {
+          case "isRunning":
+            $(Simulator.toggler).prop('indeterminate', false);
+            if (message.isRunning) {
+              $(Simulator.toggler).prop('checked', true);
+            }
+            else {
+              $(Simulator.toggler).prop('checked', false);
+            }
+            break;
+          case "listApps":
+            $('#apps-dir').val(message.dir);
+            $('#apps-list').text(JSON.stringify(message.list));
+            break;
+        }
+      },
+      false
+    );
+    window.postMessage({ name: "getIsRunning" }, "*");
+    window.postMessage({ name: "listApps" }, "*");
+  },
+
+  show: function(target) {
     var to = $(target)[0];
-    if (to == section) {
-      return;
-    }
-    $(section).hide();
-    $('a[href="#' + $(section).attr('id') + '"]').removeClass('active');
-    section = to;
-    $(section).show();
-    $('a[href="#' + $(section).attr('id') + '"]').addClass('active');
-  });
-
-  document.documentElement.addEventListener(
-    "addon-message",
-    function isRunningListener(event) {
-      var message = event.detail;
-      if (!("name" in message)) {
+    if (this.section) {
+      if (to == this.section) {
         return;
       }
-      switch (message.name) {
-        case "isRunning":
-          if (message.isRunning) {
-            $("#simulatorStatus").text("Running");
-            $("#toggleButton").addClass("started");
-          }
-          else {
-            $("#simulatorStatus").text("Stopped");
-            $("#toggleButton").removeClass("started");
-          }
-          break;
-        case "listApps":
-          $('#apps-dir').val(message.dir);
-          $('#apps-list').text(JSON.stringify(message.list));
-          break;
-      }
-    },
-    false
-  );
-  window.postMessage({ name: "getIsRunning" }, "*");
-  window.postMessage({ name: "listApps" }, "*");
+      $(this.section).hide();
+      $('a[href="#' + $(this.section).attr('id') + '"]').removeClass('active');
+    }
+    this.section = to;
+    $(this.section).show();
+    $('a[href="#' + $(this.section).attr('id') + '"]').addClass('active');
+  },
 
-});
-
-var simulator = {
   toggle: function() {
+    $(this.toggler).prop('indeterminate', true);
     window.postMessage({ name: "toggle" }, "*");
   },
+
   create: function() {
     window.postMessage({ name: "create" }, "*");
-  },
+  }
+
 };
+
+$(document).ready(function() {
+  Simulator.init();
+});
+
