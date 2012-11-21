e7853fc08c165e01337fbe9b8ed3c1f11ced1ad0
Myk Melez <myk@mozilla.org>
2012-11-20 16:19:05 -0800

    set marionette.defaultPrefs.enabled to false to get remote debugger working; remove devtools.debugger.remote-port to use default port (6000)

--- prosthesis/defaults/preferences/prefs.js
+++ prosthesis/defaults/preferences/prefs.js
@@ -1,4 +1,4 @@
 user_pref("general.useragent.override", "Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0");
 user_pref("power.screen.timeout", 86400);
 user_pref("devtools.debugger.remote-enabled", true);
-user_pref("devtools.debugger.remote-port", 4242);
+user_pref("marionette.defaultPrefs.enabled", false);
