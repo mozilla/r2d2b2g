2e8132362a65e14a041e5b8409800e4c6f427852
Myk Melez <myk@mozilla.org>
2012-12-11 16:36:55 -0800

    update b2g to most recent nightly, 2012-12-10

--- build/default.mk
+++ build/default.mk
@@ -1,4 +1,4 @@
 # A recent stable nightly Beta build of B2G.
 # Sometimes this is based on the latest stable nightly for Unagi according to
 # https://releases.mozilla.com/b2g/promoted_to_stable/ (private URL).
-DATE = 2012-11-28
+DATE = 2012-12-10
