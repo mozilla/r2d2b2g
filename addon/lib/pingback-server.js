b9758e46aa5c5e9690c4a852a55b2fa40bda9a32
Luca Greco <luca.greco@alcacoop.it>
2012-12-24 08:16:30 +0100

    added license header and 'use strict' on all addon/lib/*.js files

--- addon/lib/pingback-server.js
+++ addon/lib/pingback-server.js
@@ -1,9 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+
+'use strict';
+
 const { Cc, Ci, Cr, Cu } = require("chrome");
 
 const { EventTarget } = require("sdk/event/target");
 const { emit } = require("sdk/event/core");
 const { Class } = require("sdk/core/heritage");
-const { setTimeout, clearTimeout } = require('sdk/timers')
+const { setTimeout, clearTimeout } = require('sdk/timers');
 
 const PingbackServer = Class({
   extends: EventTarget,
