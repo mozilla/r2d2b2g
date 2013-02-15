0d8fc612803bce1f2a77d41dd7c49dfc3d1dab71
Luca Greco <luca.greco@alcacoop.it>
2013-02-15 19:08:30 +0100

    disable lockscreen on simulator b2g-desktop instances

--- prosthesis/content/shell.js
+++ prosthesis/content/shell.js
@@ -2,6 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// NOTE: disalbe the lockscreen by default on FirefoxOS Simulator
+SettingsListener.observe("lockscreen.enabled", true, function(value) {
+  navigator.mozSettings
+    .createLock().set({'lockscreen.enabled': false});
+});
+
 document.getElementById("homeButton").addEventListener("mousedown", function() {
   let event = document.createEvent("KeyboardEvent");
   event.initKeyEvent("keydown", true, true, null, false, false, false, false,
