7ba690fb8e74a64a2ea305c47dc5e2e0fbcf99ec
Luca Greco <luca.greco@alcacoop.it>
2013-03-29 17:55:40 +0100

    disable prosthesis chrome logging by default

--- prosthesis/content/shell.js
+++ prosthesis/content/shell.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const DISABLE_DEBUG = false;
+const DISABLE_DEBUG = true;
 
 let debug = DISABLE_DEBUG ? function () {} : function debugSimulator() {
   let tag = Components.stack.caller ?
