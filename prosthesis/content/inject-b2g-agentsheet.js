dec35c4fed7cb97485396ce19c68c31f11f6b9d9
Myk Melez <myk@mozilla.org>
2013-04-03 16:23:16 -0700

    add license header; fix code style nit

--- prosthesis/content/inject-b2g-agentsheet.js
+++ prosthesis/content/inject-b2g-agentsheet.js
@@ -1,5 +1,9 @@
-const B2G_AGENTSHEET_URL = Services.io.newURI("chrome://prosthesis/content/b2g.css",
-                                              null, null);
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const B2G_AGENTSHEET_URL =
+  Services.io.newURI("chrome://prosthesis/content/b2g.css", null, null);
 
 // inject b2g mobile agent stylesheet into
 // all next content DOM windows created
