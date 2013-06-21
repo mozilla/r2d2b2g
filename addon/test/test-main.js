9a12215a369df24417446ba9185d2eaeec0f7f8e
Myk Melez <myk@mozilla.org>
2013-06-21 10:31:40 -0700

    add license header

--- addon/test/test-main.js
+++ addon/test/test-main.js
@@ -1,5 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 exports.test_run = function(test) {
   const main = require("main");
   test.pass("Unit test running!");
 };
-
