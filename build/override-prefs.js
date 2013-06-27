928602faee616e72c6b476bb8fa598e8adcbf095
Myk Melez <myk@mozilla.org>
2013-06-26 21:33:44 -0700

    check battery.charging before shutting down instead of disabling battery API

--- build/override-prefs.js
+++ build/override-prefs.js
@@ -2,10 +2,3 @@
 // prosthesis/defaults/preferences/prefs.js, but these load earlier, so this
 // file is useful for prefs that are accessed before addons are loaded.
 
-// Disable the battery API so Gaia doesn't shut down the Simulator on machines
-// with a dead or missing battery that it misinterprets as a low power state.
-//
-// This has to be here to affect the state of BatteryManager, which checks it
-// before addons have loaded.
-//
-user_pref("dom.battery.enabled", false);
