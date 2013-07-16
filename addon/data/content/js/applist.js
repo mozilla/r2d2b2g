ff4a8edafd9358ea6691e7ac1f5eaff11602a0e3
Myk Melez <myk@mozilla.org>
2013-07-16 14:35:13 -0700

    update comment and be stricter about what constitutes an absolute URL

--- addon/data/content/js/applist.js
+++ addon/data/content/js/applist.js
@@ -93,8 +93,10 @@ var AppList = (function() {
         var iconPath = "default.png";
 
         if (app.icon) {
-            if ((app.icon.indexOf("data:image/") === 0) || (app.icon.indexOf('http') === 0)) {
-                // set the src to the data uri
+            if ((app.icon.indexOf("data:image/") === 0) ||
+                (app.icon.indexOf('http:') === 0) ||
+                (app.icon.indexOf('https:') === 0)) {
+                // set the src to the data: or absolute URL
                 iconPath = app.icon;
             } else if (app.type === "hosted") {
                 // hosted app icon is relative to the app origin
