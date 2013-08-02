11da95bfa84c85dcdd4d990f83680d2cab9766e2
Brandon Kase <bkase@cmu.edu>
2013-08-02 11:54:59 -0700

    Make clean on Windows cleans everything

--- android-tools/adb-bin/make-win.bat
+++ android-tools/adb-bin/make-win.bat
@@ -1,5 +1,5 @@
 del "..\win-out\*.*" /F /Q
-echo "." > "..\win-out\.exist"
+echo|set /p=. > "..\win-out\.exist"
 
 set files=adb.cpp adb_client.cpp file_sync_client.cpp services.cpp transport.cpp transport_local.cpp transport_usb.cpp usb_vendors.cpp utils.cpp exports.cpp usb_windows.cpp get_my_path_windows.cpp declare_array_lists.cpp sysdeps_win32.cpp ..\\libcutils\\list.c
 set objs="..\\win-out\\sockets.obj" "..\\win-out\\adb.obj" "..\\win-out\\adb_client.obj" "..\\win-out\\file_sync_client.obj" "..\\win-out\\services.obj" "..\\win-out\\transport.obj" "..\\win-out\\transport_local.obj" "..\\win-out\\transport_usb.obj" "..\\win-out\\usb_vendors.obj" "..\\win-out\\utils.obj" "..\\win-out\\exports.obj" "..\\win-out\\usb_windows.obj" "..\\win-out\\get_my_path_windows.obj" "..\\win-out\\sysdeps_win32.obj" "..\\win-out\\socket_local_client.obj" "..\\win-out\\socket_local_server.obj" "..\\win-out\\list.obj" "..\\win-out\\declare_array_lists.obj"
